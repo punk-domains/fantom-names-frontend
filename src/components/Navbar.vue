134e02926f311cdc180ac6c2a51437eb757fa4fc
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-12-12 01:16:15 +0100

    minor logo changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand">
+        <img src="../assets/fantom-logo.svg" alt="" height="30" class="d-inline-block align-bottom navbar-img">
         Fantom Names
       </router-link>
 
